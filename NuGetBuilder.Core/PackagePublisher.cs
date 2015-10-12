95c7969828256d41ad6c176e2e31541bbbf3ac77
Michael <michael.star.mail@gmail.com>
2015-10-12 15:41:58 +0400

    Code and libraries to test new package publish command

--- NuGetBuilder.Core/PackagePublisher.cs
+++ NuGetBuilder.Core/PackagePublisher.cs
@@ -4,13 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NuGet;
+using NuGet.Commands;
 
 namespace NuGetBuilder.Core
 {
     public class PackagePublisher : IPackagePublisher
     {
+        PackageServer packageServer = new PackageServer("", "");
+
         public void PublishNewPackageVersion(ManifestMetadata packageMetadata)
         {
+            string packagePath = "";
+            string nugetSource = "nuget.org";
+            packageServer.PushPackage("key", null, -1, 60 * 1000, false);
+
+            PushCommand cmd = new PushCommand();
+            cmd.Arguments.Add("C:\\Packages\\MyPkg.nupkg");
+            cmd.Source = "nuget.org";
+            cmd.ApiKey = "key";
+            cmd.Timeout = 10 * 1000;
+            cmd.Execute();
+
             throw new NotImplementedException();
         }
     }
