c6ba568169478b49aac35bb7ab07be98315a0bcc
Michael <michael@webbuild.com.ua>
2015-10-12 16:09:27 +0400

    PackageJsonVersionChecker implementation

--- NuGetBuilder.Core/PackageJsonVersionChecker.cs
+++ NuGetBuilder.Core/PackageJsonVersionChecker.cs
@@ -1,6 +1,8 @@
-﻿using NuGet;
+﻿using Newtonsoft.Json;
+using NuGet;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +13,17 @@ namespace NuGetBuilder.Core
     {
         public bool CheckVersion(SemanticVersion version, string repoPath)
         {
-            throw new NotImplementedException();
+            var packageDefinitionFilePath = Path.Combine(repoPath, "package.json");
+            if (File.Exists(packageDefinitionFilePath))
+            {
+                dynamic packageDef = JsonConvert.DeserializeObject(File.ReadAllText(packageDefinitionFilePath));
+                var gitVersion = SemanticVersion.Parse(packageDef.version);
+                return version.CompareTo(gitVersion) < 0;
+            }
+            else
+            {
+                throw new NotSupportedException("This Version Checker supports 'package.json' files only.");
+            }
         }
     }
 }
