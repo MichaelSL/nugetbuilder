b8cd4749534f6a7fbf8cfd4e9aa97eb7dab9f3b9
Michael <michael.star.mail@gmail.com>
2015-10-13 11:35:45 +0400

    Added a test for checking latest package version

---
+++ NuGetBuilder.CoreTests/NuGetPackageVersioningTests.cs
@@ -0,0 +1,18 @@
+﻿using NuGet;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace NuGetBuilder.CoreTests
+{
+    public class NuGetPackageVersioningTests
+    {
+        [Fact]
+        public void TestFindPackageCommand()
+        {
+            IPackageRepository repo = PackageRepositoryFactory.Default.CreateRepository("http://go.microsoft.com/fwlink/?LinkID=206669");
+            var package = repo.FindPackage("maskInput");
+            Assert.True(package.IsLatestVersion);
+        }
+    }
+}
